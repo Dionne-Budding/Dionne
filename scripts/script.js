de9798e27683f8ead076c498d521f742db0ea88e
Dionne-Budding <74144758+Dionne-Budding@users.noreply.github.com>
2020-12-09 15:54:34 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,59 @@
 // JavaScript Document
-var start = document.getElementById("werknemers");
-var knop = document.getElementById("klanten");
+// console.log("hello");
+// var content = Array.from(document.querySelectorAll('.content'));
+//
+// var contentCount = myContent.length;
+//
+// var currentContent = 0;
+//
+// var previous = document.getElementById("previous");
+// var next = document.getElementById("next");
+//
+// function removeAllActive(){
+//   "use strict";
+//   myContent.forEach(function (item) {
+//     item.classList.remove('active-card', 'rotate-vert-center');
+//   });
+// }
+//
+// theChecker();
+// //previous button checker
+// function prevContent() {
+//   "use strict";
+//   if(previous.classList.contains('disabled')) {
+//     // do nothing
+//     return false;
+//   } else {
+//     currentContent--;
+//     theChecker();
+//   }
+// }
+//
+// function nextContent() {
+//   "use strict";
+//   if(next.classList.contains('disabled')) {
+//     // do nothing
+//     return false;
+//   } else {
+//     currentContent++;
+//     theChecker();
+//   }
+// }
+//
+// previous.onclick=prevContent;
+// next.onclick=nextContent;
+//
+// function  theChecker(){
+//   "use strict";
+//   //remove all active
+//      removeAllActive();
+//   //set active class on current card
+//   myContent[currentContent].classList.add('active-card', 'rotate-vert-center');}
+var menu = document.getElementById("klik");
+
+function klikken(){
+  var lijst = document.getElementById("menu");
+  lijst.classList.toggle("change");
+}
+
+klik.addEventListener("click", klikken);
