465e7f51038945cb54f64a6a2bdf3e2575201594
Dionne-Budding <74144758+Dionne-Budding@users.noreply.github.com>
2021-04-28 12:36:48 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,2 @@
-var navbar = document.querySelector(".navbar");
-var ham = document.querySelector(".ham");
-
-
-function hamburger() {
-  navbar.classList.toggle("open")
-  ham.classList.toggle("dicht")
-}
-
-
-ham.addEventListener("click", hamburger);
+// JavaScript Document
+console.log("howdy");
